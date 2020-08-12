228f2f5e76bfc834bea27099e4263f998b3bb504
user <m.ivans1999@gmail.com>
2020-08-12 15:04:19 +0300

    20200811_17_25

---
+++ test_fib.sh
@@ -0,0 +1,17 @@
+clear
+  echo "Cik daudz Fibonaci skaitlus gribi paradit?"
+  read n
+  x=0
+  y=1
+  i=2
+  echo "Fibonacci serija lidz  $n :"
+  echo "$x"
+  echo "$y"
+  while [ $i -lt $n ]
+  do
+      i=`expr $i + 1 `
+      z=`expr $x + $y `
+      echo "$z"
+      x=$y
+      y=$z
+  done
